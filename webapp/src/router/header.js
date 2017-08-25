05071eb747dd96d43ed75d3c355027eb208af2d1
dinghy <dinghy2412@163.com>
2017-08-25 14:14:53 +0800

    test

--- webapp/src/router/header.js
+++ webapp/src/router/header.js
@@ -13,9 +13,9 @@ class Header extends React.Component {
         return (
             <header>
                 <div className="headerBox clearfix">
-                    <svg className="icon weixin" aria-hidden="true">
-                        <use xlinkHref="#icon-welcomezhuanhuan"></use>
-                    </svg>
+                    {/*<svg className="icon weixin" aria-hidden="true">*/}
+                        {/*<use xlinkHref="#icon-welcomezhuanhuan"></use>*/}
+                    {/*</svg>*/}
                     {/*<iframe frameBorder="no" marginWidth="0" marginHeight="0" width='298' height='52' src="//music.163.com/outchain/player?type=2&id=31165295&auto=0&height=32"></iframe>*/}
                 </div>
             </header>
