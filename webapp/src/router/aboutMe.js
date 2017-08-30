3e2e64e2947e6ee7d731d07012a265f74e036364
dinghy <dinghy2412@163.com>
2017-08-30 17:34:51 +0800

    调整移动端首页高度分布

--- webapp/src/router/aboutMe.js
+++ webapp/src/router/aboutMe.js
@@ -6,12 +6,25 @@ import Footer from '../router/footer'
 class AboutMe extends React.Component {
     constructor (props) {
         super(props);
+        if (window.screen.width < 768) {
+            this.state.style = {
+                backgroundImage: 'url("//img.dinghyu.com/20990657_1368686545127.jpg?imageView2/0/w/750")',
+                backgroundSize : 'cover'
+
+            }
+        }
     }
+
+    state = {
+        style : {
+            background: 'url("//img.dinghyu.com/20990657_1368686545127.jpg?imageslim") no-repeat'
+        }
+    };
 
     render () {
         return (
             <section id="aboutMe">
-                <div className="infoBox">
+                <div className="infoBox" style={this.state.style}>
                     <div className="wxBox">
                         <img className="wxIcon" src="//img.dinghyu.com/weixin2.png" alt="微信icon"/>
                         <div>Add WECHAT TO CHAT</div>
